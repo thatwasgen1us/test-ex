efacf3a0971f1734de26d2f2ac812592d2ca0f83
Егор Половко <skelletrise@gmail.com>
2024-12-22 01:13:04 +0700

    Complete Header and Footer

---
+++ scripts/Header.js
@@ -0,0 +1,36 @@
+class Header {
+  selectors = {
+    root: "[data-js-header]",
+    menu: "[data-js-header-menu]",
+    burgerButton: "[data-js-header-burger-button]",
+  };
+
+  stateClasses = {
+    isActive: "is-active",
+    isLock: "is-lock",
+  };
+
+  constructor() {
+    this.rootElement = document.querySelector(this.selectors.root);
+    this.menuElement = this.rootElement.querySelector(this.selectors.menu);
+    this.burgerButtonElement = this.rootElement.querySelector(
+      this.selectors.burgerButton
+    );
+    this.bindEvents();
+  }
+
+  onBurgerButtonClick = () => {
+    this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+    this.menuElement.classList.toggle(this.stateClasses.isActive);
+    document.documentElement.classList.toggle(this.stateClasses.isLock);
+  };
+
+  bindEvents() {
+    this.burgerButtonElement.addEventListener(
+      "click",
+      this.onBurgerButtonClick
+    );
+  }
+}
+
+export default Header;
