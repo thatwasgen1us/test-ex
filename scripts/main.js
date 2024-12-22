eea3c2a0571d7d944aa56fedbcfc287333119f8b
Егор Половко <skelletrise@gmail.com>
2024-12-23 02:51:06 +0700

    map complete

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,16 @@ new Swiper(".swiper", {
   },
   loop: true,
 });
+
+function init() {
+  const map = new ymaps.Map("map", {
+    center: [55.028894, 82.926493],
+    zoom: 15,
+  });
+  const placemark = new ymaps.Placemark([55.028894, 82.926493], {
+    balloonContent: "Новосибирск, Депутатская, 46",
+  });
+
+  map.geoObjects.add(placemark);
+}
+ymaps.ready(init);
